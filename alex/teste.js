e57ffcbf569e5aaf7fff8abdfc51959548392ba0
Alexssandro Bianchi <alexssandro.bianchi@intelltech.com.br>
2019-12-09 11:35:11 -0300

    Teste2

---
+++ alex/teste.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const teste = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+teste.propTypes = {
+
+}
+
+export default teste
